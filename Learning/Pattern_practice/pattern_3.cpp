1f7dec61956411e84f86e6f13c9e63e2524bf93d
chahat sagar <109112505+chahatsagarmain@users.noreply.github.com>
2023-01-08 00:31:32 +0530

    Added comments

--- Learning/Pattern_practice/pattern_3.cpp
+++ Learning/Pattern_practice/pattern_3.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 using namespace std;
-
+/**
+**
+***
+****
+*****
+****
+***
+**
+*
+*/
 int main(){
     int n = 10;
+    //using this loop to print rows
     for (int i = 0; i < 10 ; i++){
+        //till we reach the half part , we will print stars in incresing order
         if(i < 5){
             for(int j = 0 ; j < i + 1 ; j++){
                 printf("*");
             }
         
         }
+        //after we reach half , we will print stars in decreasing order
+        
         else { 
             for(int j = n ; j > i+ 1   ; j--){
                 printf("*");
             }
         }
+        //after completing the fiest row , we will go to the next line
         printf("\n");
     }
 }
